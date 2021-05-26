287c5793e976057fa7f798371633ef27651a05f6
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-05-26 02:54:59 -0400

    commit tree 5ae1f02e4a4c86f85dc791ed2f7f5e31f5af0a0c

--- src/App.js
+++ src/App.js
@@ -1,1 +1,24 @@
 // create your App component here
+import React from 'react'
+
+class App extends React.Component{
+
+    state= {
+        astros: []
+            }
+
+    componentDidMount () {
+        fetch( "http://api.open-notify.org/astros.json")
+        .then (r => r.json())
+        .then(astrosData => this.setState({astros: astrosData}))
+    }
+
+    render () {
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+export default App
